9381220d8d385fc360ca04f6c242cf1e1c67d032
Niels J <80054446+PappaNiels@users.noreply.github.com>
2022-04-04 13:41:22 +0200

    Update security.lua

--- security.lua
+++ security.lua
@@ -1,14 +1,35 @@
 isInSecurity = false
 
+local blipActive = false
 local blipsThreeKeypad = false 
 local blipOne
 local blipTwo 
-local blip
+local blip = {1, 1}
 
 RegisterCommand("sec_blips", function()
     AddSecurityBlips()
 end, false)
 
 function AddSecurityBlips()
-
+    for i = 1, #blip, 1 do 
+        blip[i] = AddBlipForCoord(lvlFourKeypad[1])    
+        SetBlipSprite(blip[i], 733)
+        SetBlipColour(blip[i], 2)
+        SetBlipHighDetail(blip[i], true)
+    end
 end
+
+CreateThread(function()
+    while true do 
+        if isInSecurity then 
+            if not blipActive then 
+                AddSecurityBlips()
+                    Wait(100)
+            else
+                    
+            end
+        else 
+                Wait(500)
+        end
+    end
+end)
