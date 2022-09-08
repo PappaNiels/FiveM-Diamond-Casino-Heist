369cab47f264031ab5a69892f7e82acf2ddf2882
pindakaastosti <pindakaastosti111@gmail.com>
2022-09-08 21:26:13 +0200

    fix(coords) *correct* coords

--- shared/coords.lua
+++ shared/coords.lua
@@ -6,17 +6,17 @@
 ]]
 
 entryCoords = {
-    vector3(993.17, 77.05, 80.99),     -- Waste Disposal
-    vector3(923.76, 47.20, 81.11),     -- Main Entrance
-    vector3(936.42, 14.51, 112.55),    -- Roof South West
-    vector3(972.15, 25.54, 120.24),    -- Roof Helipad North  
-    vector3(953.40, 79.20, 111.25),    -- Roof North West
-    vector3(1000.4, -54.99, 74.96),    -- Security Tunnel
-    vector3(893.29, -176.47, 22.58),   -- Sewer
+    vector3(993.17, 77.05, 80.99),     -- Waste Disposal 
+    vector3(923.76, 47.20, 81.11),     -- Main Entrance 
+    vector3(936.42, 14.51, 112.55),    -- Roof South West 
+    vector3(972.15, 52.54, 120.24),    -- Roof Helipad North 
+    vector3(953.40, 79.20, 111.25),    -- Roof North West 
+    vector3(1000.4, -54.99, 74.96),    -- Security Tunnel 
+    vector3(893.29, -176.47, 22.58),   -- Sewer 
     vector3(953.78, 4.02, 111.26),     -- Roof South East 
     vector3(959.39, 31.75, 120.23),    -- Roof Helipad South 
-    vector3(988.32, 59.03, 111.26),    -- Roof North East
-    vector3(978.78, 18.64, 80.99)      -- Staff Lobby
+    vector3(988.32, 59.03, 110.26),    -- Roof North East 
+    vector3(978.78, 18.64, 80.99)      -- Staff Lobby 
 }
 
 -- These are the coordinates where the cargobob spawns (first vector) and where it flies to (second vector).
