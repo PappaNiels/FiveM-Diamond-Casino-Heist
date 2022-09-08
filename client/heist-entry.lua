82a628d9217a05c058ed2ee972f2f391aab25f32
pindakaastosti <pindakaastosti111@gmail.com>
2022-09-08 20:04:04 +0200

    tweak(entry) cargobob task

--- client/heist-entry.lua
+++ client/heist-entry.lua
@@ -7,11 +7,52 @@
 
 local cargobob = {}
 
+local function FlyToPos()
+    TaskHeliMission(cargobob[2], cargobob[1], 0, 0, cargobobFinalCoords, 4, 10.0, 0.0, 300.0, 9, 7, -1.0, 25280)
+    
+    while #(GetEntityCoords(cargobob[1]) - cargobobFinalCoords) > 500 do 
+        Wait(100)
+    end
+    
+    DoScreenFadeOut(800)
+    
+    while not IsScreenFadedOut() do 
+        Wait(500)
+    end
+
+    DeleteVehicle(cargobob[1])
+    DeleteEntity(cargobob[2])
+    RemoveBlip(cargobob[3])
+
+    cargobob[1] = CreateVehicle(GetHashKey("cargobob2"), cargobobFinalCoords, 250.47, true, false)
+    SetVehicleColours(cargobob[1], 0, 0)
+    
+    cargobob[2] = CreatePed(0, GetHashKey("s_m_y_pilot_01"), cargobobFinalCoords, 0, false, false)
+    SetPedIntoVehicle(cargobob[2], cargobob[1], -1)
+    SetVehicleEngineOn(cargobob[1], true, true, true)
+    
+    SetModelAsNoLongerNeeded("cargobob2")
+    SetModelAsNoLongerNeeded("s_m_y_pilot_01")
+
+    SetVehicleDoorOpen(cargobob[1], 2, false, true)
+
+    FreezeEntityPosition(cargobob[1], true)
+    
+    Wait(5000)
+
+
+    SetEntityCoords(PlayerPedId(), 1305.6, -70.52, 298.0, true, false, false, false)
+    SetEntityHeading(PlayerPedId(), 69.19)
+    
+    DoScreenFadeIn(1000)
+    FreezeEntityPosition(cargobob[1], false)
+end
+
 local function IsAllPlayersInHeli()
     local bool = true
     while bool do 
         DisableControlAction(0, 23, true)
-
+        
         if IsPedInAnyHeli(GetPlayerPed(GetPlayerFromServerId(hPlayer[1]))) and IsPedInAnyHeli(GetPlayerPed(GetPlayerFromServerId(hPlayer[2]))) and IsPedInAnyHeli(GetPlayerPed(GetPlayerFromServerId(hPlayer[3]))) and IsPedInAnyHeli(GetPlayerPed(GetPlayerFromServerId(hPlayer[4]))) then 
             FlyToPos()
             bool = false 
@@ -22,6 +63,8 @@ end
 local function SetupCargobob()
     LoadModel("cargobob2")
     LoadModel("s_m_y_pilot_01")
+    
+    print(hPlayer[2])
 
     cargobob[1] = CreateVehicle(GetHashKey("cargobob2"), cargobobCoords, 328.34, true, false)
     SetVehicleColours(cargobob[1], 0, 0)
@@ -29,7 +72,7 @@ local function SetupCargobob()
     --SetCarBootOpen(cargobob[1])
     --SetVehicleEngineOn(cargob, true, true, false)
 
-    cargobob[2] = CreatePed(0, GetHashKey("s_m_y_pilot_01"), GetEntityCoords(cargobob[1]), 0, false, false)
+    cargobob[2] = CreatePed(0, GetHashKey("s_m_y_pilot_01"), cargobobCoords, 0, false, false)
     SetEntityForAll(cargobob[2])
     SetPedIntoVehicle(cargobob[2], cargobob[1], -1)
     SetEntityInvincible(cargobob[2], true)
@@ -40,42 +83,27 @@ local function SetupCargobob()
     SetBlipColour(cargobob[3], 54)
     SetBlipHighDetail(cargobob[3], true)
 
-    SetVehicleDoorOpen(cargobob[1], 2, false, true)
-    
+    --SetModelAsNoLongerNeeded("cargobob2")
+    --SetModelAsNoLongerNeeded("s_m_y_pilot_01")
+
+    while not IsPedInAnyHeli(PlayerPedId()) do 
+        Wait(0)
+        --print("tick")
+        if #(vector3(1060.1, -288.31, 50.81) - GetEntityCoords(PlayerPedId())) < 4 and IsControlJustPressed(0, 23) then 
+            for i = 0, 4 do 
+                if IsVehicleSeatFree(cargobob[1], i) then 
+                    TaskEnterVehicle(PlayerPedId(), cargobob[1], 1.0, i, 2.0, 0, 0)
+                    break
+                end
+            end
+        end
+    end
+
     Wait(3000)
-
---
-    --TaskHeliMission(cargobob[2], cargobob[1], 0, 0, cargobobFinalCoords, 4, 5.0, 0.0, 300.0, 9, 7, -1.0, 25280)
---
-    --while #(GetEntityCoords(cargobob[1]) - cargobobFinalCoords) > 5 do 
-    --    Wait(100)
-    --end
---
-    --SetEntityCoords(cargobob[1], cargobobFinalCoords, true, false, false, false)
-    --SetEntityHeading(cargobob[1], 278.47)
-    --FreezeEntityPosition(cargobob[1], true)
-    SetVehicleDoorOpen(cargobob[1], 3, false, true)
-
-    --TASK_HELI_MISSION(iVar1, iVar0, 0, 0, ENTITY::GET_ENTITY_COORDS(iVar0, true) + ENTITY::GET_ENTITY_FORWARD_VECTOR(iVar0) * Vector(150f, 150f, 150f) + Vector(10f, 0f, 0f), 4, 5f, 0f, ENTITY::GET_ENTITY_HEADING(iVar0), 9, 7, -1f, 25280);
-    --while not IsPedInAnyHeli(PlayerPedId()) do 
-    --    if #(cargobobCoords - GetEntityCoords(PlayerPedId())) < 5 and IsControlJustPressed(0, 23) then 
-    --        for i = 0, 4 do 
-    --            if IsVehicleSeatFree(cargobob[1], i) then 
-    --                TaskEnterVehicle(PlayerPedId(), cargobob[1], 1.0, i, 2.0, 0, 0)
-    --                break
-    --            end
-    --        end
-    --    end
-    --end
-
-    --IsAllPlayersInHeli()
-    --TaskEnterVehicle(PlayerPedId(), cargobob[1], 1.0, 1, 2.0, 0, 0)
-    --print(GetVehicleDoorsLockedForPlayer(cargobob[1]))
-end
-
-local function FlyToPos()
     
+    FlyToPos()
 end
+
 
 function StartHeist()
     local player = GetCurrentHeistPlayer()
@@ -101,4 +129,16 @@ RegisterCommand("test_vehicle", function()
     --SetPedIntoVehicle(PlayerPedId(), veh, -1)
     --SetVehicleColours(veh, 7, 7)
     SetupCargobob()
+    --FlyToPos()
+end, false)
+
+RegisterCommand("test_marker", function()
+    CreateThread(function()
+        while true do 
+            Wait(0)
+            for i = 1, #entryCoords do 
+                DrawMarker(1, entryCoords[i], 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.75, 58, 230, 242, 160, false, false, 2, false)
+            end
+        end
+    end)
 end, false)
