f33f10e66aefc70b95082ae53a8e909682166ef6
pindakaastosti <pindakaastosti111@gmail.com>
2022-09-23 16:41:03 +0200

    feat(casino) staff lobby rework

--- client/heist-casino.lua
+++ client/heist-casino.lua
@@ -1,6 +1,33 @@
 local keycardScene = 0
 local player = 0
 
+local blips = {}
+local keycardSyncAnims = {
+    {
+        {
+            {"ped_a_enter", "ped_a_enter_keycard"},
+            {"ped_a_enter_loop", "ped_a_enter_loop_keycard"},
+            {"ped_a_intro_b", "ped_a_intro_b_keycard"},
+            {"ped_a_loop", "ped_a_loop_keycard"},
+            {"ped_a_pass", "ped_a_pass_keycard"},
+            {"ped_a_fail_a", "ped_a_fail_a_keycard"}, 
+            {"ped_a_fail_b", "ped_a_fail_b_keycard"}, 
+            {"ped_a_fail_c", "ped_a_fail_c_keycard"} 
+        },
+        { 
+            {"ped_b_enter", "ped_b_enter_keycard"},
+            {"ped_b_enter_loop", "ped_b_enter_loop_keycard"},
+            {"ped_b_intro_b", "ped_b_intro_b_keycard"},
+            {"ped_b_loop", "ped_b_loop_keycard"},
+            {"ped_b_pass", "ped_b_pass_keycard"},
+            {"ped_b_fail_a", "ped_b_fail_a_keycard"}, 
+            {"ped_b_fail_b", "ped_b_fail_b_keycard"}, 
+            {"ped_b_fail_c", "ped_b_fail_c_keycard"}
+        }
+    },
+    {}
+}
+
 local function KeypadOne(j)
     local keycard = "ch_prop_vault_key_card_01a"
     local animDict = "anim_heist@hs3f@ig3_cardswipe@male@"
@@ -19,6 +46,26 @@ local function KeypadOne(j)
     Wait(3700)
     DeleteObject(keycardObj)
     ClearPedTasks(PlayerPedId())
+end
+
+local function SyncKeycardEnter(num)
+    local animDict = "anim_heist@hs3f@ig3_cardswipe_insync@male@"
+    local keycard = "ch_prop_vault_key_card_01a"
+    LoadAnim(animDict)
+    
+    if not keycardSyncAnims[2][1] then
+        local keypadObj = GetClosestObjectOfType(keypads[4][num], 1.0, GetHashKey("ch_prop_fingerprint_scanner_01d"), false, false, false)
+        local keycardObj = CreateObject(GetHashKey(keycard), GetEntityCoords(PlayerPedId()), true, true, false)
+
+        for i = 1, keycardSyncAnims[1][2] do 
+            keycardSyncAnims[2][i] = NetworkCreateSynchronisedScene(GetEntityCoords(keypadObj), GetEntityRotation(keypadObj), 2, true, false, 0, 0, 1.3)
+            NetworkAddPedToSynchronisedScene(PlayerPedId(), keycardSyncAnims[2][i], animDict, keycardSyncAnims[1][num][i][1], 4.0, -4.0, 1033, 0, 1000.0, 0)
+            NetworkAddEntityToSynchronisedScene(keycardObj, keycardSyncAnims[2][i], animDict, keycardSyncAnims[1][num][i][2], 1.0, -1.0, 114886080)
+        end
+    end
+
+    NetworkStartSynchronisedScene(keycardSyncAnims[2][1])
+
 end
 
 function RoofTerraceEntry()
@@ -117,14 +164,59 @@ function Basement()
     SetBlipSprite(blip[1], 63)
     SetBlipSprite(blip[2], 743)
 
+    local num = 1
+    local zCoord = -61
     CreateThread(function()
         while true do 
-            Wait(0)
+            Wait(100)
             
+            SubtitleMsg("Go to the ~y~basement~s~.", 110)
+
+            local coords = GetEntityCoords(PlayerPedId())
+            if coords.z < zCoord then 
+                RemoveBlip(blips[num])
+
+                if num == 1 then
+                    zCoord = -67
+                    num = 2
+                else
+                    SecurityLobby() 
+                    break
+                end
+            end
         end
     end)
 end
     
+function SecurityLobby()
+    for i = 1, 2 do 
+        blips[i] = AddBlipForCoord(keypads[4][i])
+        SetBlipSprite(blips[i], 733)
+        SetBlipColour(blips[i], 2)
+        SetBlipHighDetail(blips[i], true)
+    end
+
+    CreateThread(function()
+        while true do 
+            Wait(5)
+
+            local distanceL, distanceR = #(GetEntityCoords(PlayerPedId()) - keypads[4][1]), #(GetEntityCoords(PlayerPedId()) - keypads[4][2])
+            
+            if distanceL <  or distanceR <  then 
+                HelpMsg("Press ~INPUT_CONTEXT~ to get in position to insert the keycard.") 
+                if IsControlPressed(0, 38) then 
+                    if distanceL < distanceR then 
+                        SyncKeycardEnter(1)
+                    else
+                        SyncKeycardEnter(2)
+                    end
+                    break
+                end
+            end 
+        end
+    end)
+end
+
 RegisterNetEvent("cl:casinoheist:testCut", MainEntry)
 
 RegisterCommand("test_cut_agg", function()
