d825f0ca1fe0f89f6a8265218b9614f3e0fe0243
pindakaastosti <pindakaastosti111@gmail.com>
2022-11-06 23:00:02 +0100

    tweak(hud) rework hud

--- client/hud.lua
+++ client/hud.lua
@@ -7,27 +7,17 @@ local test = false
 local teamlivesColour = {255, 255, 255, 500}
 
 local amountSize = 0.5
-local wide = 0
-local height = 0
+local height = 0.903
+local tw = 1.35
 local takef = ""
 
 local timeExpired = 0
-local minutes = ""
-local seconds = ""
+local min = 1
+local ten = 0
+local sec = 0
 local timerColour = {255, 255, 255, 500}
 
-AddTextEntry("team", "TEAM LIVES")
-AddTextEntry("lives", "~1~")
-
-AddTextEntry("taketxt", "TAKE")
-AddTextEntry("takenr", "$~a~")
-
-AddTextEntry("timertxt", "TIME")
-AddTextEntry("timeleft", "~a~:~a~")
-
 local function StartTimer()
-    local vaultTime = GetGameTimer()
-
     local timerActive = true
 
     CreateThread(function()
@@ -35,9 +25,21 @@ local function StartTimer()
             Wait(1000)
             timeExpired = timeExpired + 1000
 
-            if timeExpired >= hacker[hackerSelected][3 + alarmTriggered] - 5000 then 
+            if sec == 0 then 
+                sec = 9
+                if ten == 0 then 
+                    ten = 5
+                    min = min - 1
+                else    
+                    ten = ten - 1
+                end
+            else 
+                sec = sec - 1
+            end
+
+            if timeExpired >= --[[hacker[hackerSelected][3 + alarmTriggered]] 60000 - 5000 then 
                 timerColour = {201, 37, 37, 255}
-                if timeExpired == hacker[hackerSelected][3 + alarmTriggered] then 
+                if timeExpired == --[[hacker[hackerSelected][3 + alarmTriggered]] 60000 then 
                     timerActive = false
                 end
             end
@@ -45,116 +47,94 @@ local function StartTimer()
     end)
 end
 
-local function FormatTimer()
-    local time = math.floor((hacker[hackerSelected][3 + alarmTriggered] - timeExpired)/ 1000) 
-
-    if time < 60 then 
-        minutes = "00"
-        if time < 10 then 
-            seconds = "0"..tostring(time)
-        else 
-            seconds = tostring(time)
-        end
-    elseif time >= 60 and time < 120 then 
-        minutes = "01"
-        if time - 60 < 10 then 
-            seconds = "0"..tostring(time - 60)
-        else 
-            seconds = tostring(time - 60)
-        end
-    elseif time >= 120 and time < 180 then 
-        minutes = "02"
-        if time - 120 < 10 then 
-            seconds = "0"..tostring(time - 120)
-        else 
-            seconds = tostring(time - 120)
-        end
-    elseif time >= 180 then 
-        minutes = "03"
-        if time - 180 < 10 then 
-            seconds = "0"..tostring(time - 180)
-        else 
-            seconds = tostring(time - 180)
-        end
-    end
-end
-
 local function FormatTake()
+    -- 4 = 1.285 5 = 1.2675 6 = 1.25 7 = 1.253
+    --local take = 000
     local len = string.len(tostring(take))
 
     if len == 4 then 
         takef = string.sub(take, 1, 1) .. "," .. string.sub(take, 2, 4)
+        height = 0.903
+        amountSize = 0.5
     elseif len == 5 then 
         takef = string.sub(take, 1, 2) .. "," .. string.sub(take, 3, 5)
-        wide = 0.007
+        height = 0.903
+        amountSize = 0.5
     elseif len == 6 then 
         takef = string.sub(take, 1, 3) .. "," .. string.sub(take, 4, 6)
-        wide = 2 * 0.007
+        height = 0.903
+        amountSize = 0.5
     elseif len == 7 then 
         takef = string.sub(take, 1, 1) .. "," .. string.sub(take, 2, 4) .. "," .. string.sub(take, 5, 7)
-        wide = 0.012
-        height = 0.005
+        height = 0.908
         amountSize = 0.4
     end
 end
 
---function hud for one
-
 local function DrawTeamlives() 
     SetTextColour(teamlivesColour[1], teamlivesColour[2], teamlivesColour[3], teamlivesColour[4])
     SetTextScale(0.28, 0.28)
-    BeginTextCommandDisplayText("team")
-    EndTextCommandDisplayText(0.853, 0.954)
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentSubstringPlayerName("TEAM LIVES")
+    EndTextCommandDisplayText(GetXTextPlace(1.126), 0.954)
 
     SetTextColour(teamlivesColour[1], teamlivesColour[2], teamlivesColour[3], teamlivesColour[4])
     SetTextScale(0.47, 0.47)
-    BeginTextCommandDisplayText("lives")
-    AddTextComponentInteger(teamlives)
-    EndTextCommandDisplayText(0.977, 0.945)
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentSubstringPlayerName(tostring(teamlives))
+    EndTextCommandDisplayText(GetXTextPlace(tw), 0.945)
     
-    if teamlives < 1 then 
-        DrawSprite("timerbars", "all_red_bg", 0.8455, 0.962, 0.29, 0.035, 0.0, 79, 12, 12, 300)
+    if teamlives > 1 then 
+        DrawSpriteCut("timerbars", "all_red_bg", 1.094, 0.962, 600.0, 65.0, 255)
     else 
-        DrawSprite("timerbars", "all_black_bg", 0.915, 0.962, 0.15, 0.035, 0.0, 100, 100, 100, 150)
+        DrawSpriteCut("timerbars", "all_black_bg", 1.232, 0.962, 300.0, 65.0, 140)
     end 
 end
 
 local function DrawTake()
-    --FormatTake()
     SetTextScale(0.28, 0.28)
-    BeginTextCommandDisplayText("taketxt")
-    EndTextCommandDisplayText(0.88, 0.915)
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentSubstringPlayerName("TAKE")
+    EndTextCommandDisplayText(GetXTextPlace(1.174), 0.915)
 
+    BeginTextCommandGetWidth("STRING")
+    AddTextComponentSubstringPlayerName("$" .. takef) -- 1.2
+    w = EndTextCommandGetWidth(1)
+    
     SetTextScale(0.0, amountSize)
-    BeginTextCommandDisplayText("takenr")
-    AddTextComponentSubstringPlayerName(takef)
-    EndTextCommandDisplayText(0.938 - wide, 0.903 + height)
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentSubstringPlayerName("$" .. takef)
+    EndTextCommandDisplayText(GetXTextPlace(-0.5226 * w + 1.3458), height)
 
-    DrawSprite("timerbars", "all_black_bg", 0.915, 0.922, 0.15, 0.035, 0.0, 100, 100, 100, 150)
+    DrawSpriteCut("timerbars", "all_black_bg", 1.232, 0.922, 300.0, 65.0, 140)
 end
 
 local function DrawTimer()
-    FormatTimer()
     SetTextColour(timerColour[1], timerColour[2], timerColour[3], timerColour[4])
     SetTextScale(0.28, 0.28)
     BeginTextCommandDisplayText("timertxt")
     EndTextCommandDisplayText(0.8815, 0.875)
 
     SetTextColour(timerColour[1], timerColour[2], timerColour[3], timerColour[4])
+    
+    BeginTextCommandGetWidth("STRING")
+    AddTextComponentSubstringPlayerName("0" .. min .. ":" .. ten .. sec)
+    w = EndTextCommandGetWidth(1)
+    
     SetTextScale(0.5, 0.5)
-    BeginTextCommandDisplayText("timeleft")
-    AddTextComponentSubstringPlayerName(minutes)
-    AddTextComponentSubstringPlayerName(seconds)
-    EndTextCommandDisplayText(0.945, 0.864)
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentSubstringPlayerName("0" .. min .. ":" .. ten .. sec)
+    EndTextCommandDisplayText(GetXTextPlace(-0.8097 * w + 1.3673181), 0.864)
 
-    DrawSprite("timerbars", "all_black_bg", 0.915, 0.882, 0.15, 0.035, 0.0, 100, 100, 100, 150)
+    DrawSpriteCut("timerbars", "all_black_bg", 1.232, 0.882, 300.0, 65.0, 140)
 end
 
 RegisterNetEvent("cl:casinoheist:syncteamlives", function(lives)
     teamlives = lives
 
     if teamlives < 1 then 
-        teamlivesColour = {201, 37, 37, 255}
+        tw = 1.347
+        teamlivesColour = {201, 35, 37, 255}
     end
 end)
 
@@ -221,7 +201,7 @@ end)
 RegisterCommand("test_sec", function() 
     LoadTexture("timerbars")
     FormatTake()
-    FormatTimer()
+    --FormatTimer()
     --test = true
     loadTeamlives = true 
     loadTake = true 
