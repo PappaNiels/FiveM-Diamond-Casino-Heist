765a71a93c01ed2833579446a97f8ac8105256cd
pindakaastosti <pindakaastosti111@gmail.com>
2022-11-17 22:54:35 +0100

    feat(exit) getaway + buyer cars + peds

--- client/heist-exit.lua
+++ client/heist-exit.lua
@@ -1,5 +1,6 @@
 local vehs = {}
 local blips = {}
+local peds = {}
 
 function VaultLobby(blip, old)
     --RemoveAllBlips()
@@ -120,7 +121,7 @@ function GoToExit()
             Wait(100)
 
             if #(GetEntityCoords(PlayerPedId()) - casinoEntryCoords[selectedExit][1][1]) < 3 then 
-                if IsNotClose(exitCoords[selectedExit][1][1], 3) then 
+                if IsNotClose(casinoEntryCoords[selectedExit][1][1], 3) then 
                     SubtitleMsg("Wait for your team members", 110)
                 else 
                     ExitCasino()
@@ -134,54 +135,166 @@ function GoToExit()
 end
 
 function ExitCasino()
-    DoScreenFadeOut(800)
+    DoScreenFadeIn(1000)
+    player = 1
+    selectedExit = 11
+    selectedDriver = 1
+    selectedVehicle = 4
+    selectedBuyer = 3
+    DoScreenFadeOut(2000)
 
     while not IsScreenFadedOut() do 
-        Wait(0)
+        Wait(10)
     end
 
-    SetEntityCoords(PlayerPedId(), 0, 0, 0, true, false, false, true)
+    SetEntityCoords(PlayerPedId(), entryCoords[selectedExit], true, false, false, true)
     SetEntityHeading(PlayerPedId(), 0)
 
-    DoScreenFadeIn(800)
-
+    
     LoadCutscene("hs3f_all_esc")
     StartCutscene(0)
-
+    DoScreenFadeIn(0)
+    
     repeat Wait(10) until HasCutsceneFinished()
-
-    if player == 1 then 
-        veh[1] = CreateVehicle(GetHashKey(availableVehicles[selectedDriver][1][selectedVehicle][1]), 982.61, -211.15, 70.38, 57.54, true, false)
-        veh[2] =CreateVehicle(GetHashKey(availableVehicles[selectedDriver][1][selectedVehicle][1]), 992.93, -214.45, 69.83, 238.67, true, false)
+    
+    DoScreenFadeOut(1)
+    
+    while not IsScreenFadedOut() do 
+        Wait(10)
     end
 
+    if player == 1 then 
+        LoadModel(availableVehicles[selectedDriver][1][selectedVehicle][1])
+        print(print(availableVehicles[selectedDriver][1][selectedVehicle][1]))
+        vehs[1] = CreateVehicle(GetHashKey(availableVehicles[selectedDriver][1][selectedVehicle][1]), 982.61, -211.15, 70.38, 57.54, true, false)
+        vehs[2] = CreateVehicle(GetHashKey(availableVehicles[selectedDriver][1][selectedVehicle][1]), 992.93, -214.45, 69.83, 238.67, true, false)
+    end
+    
     for i = 1 , 2 do 
         blips[i] = AddBlipForCoord(getawayVehicles[i])
         SetBlipSprite(blips[i], 225)
         SetBlipColour(blips[i], 54)
     end
+    
+    DoScreenFadeIn(2000)
+    
+    while not IsPedInAnyVehicle(GetHeistPlayerPed(hPlayer[1]), true) and not IsPedInAnyVehicle(GetHeistPlayerPed(hPlayer[2]), true) and not IsPedInAnyVehicle(GetHeistPlayerPed(hPlayer[3]), true) and not IsPedInAnyVehicle(GetHeistPlayerPed(hPlayer[4]), true) do 
+        Wait(100)
+        
+        if IsPedInAnyVehicle(PlayerPedId(), true) then 
+            SubtitleMsg("Wait for your team to get into a vehicle", 110)
+        else
+            SubtitleMsg("Deliver the ~g~loot~s~ to the ~y~buyer", 110)
+        end 
+    end
+    
+    RemoveBlip(blips[2])
+    RemoveBlip(blips[1])
+    
+    if player == 1 then 
+        print(print(" yesy"))
+        TriggerServerEvent("sv:casinoheist:syncMeet", false)
+    end
+end
 
+local function Route(meet)
+    local pedModels = {
+        --{"g_m_m_armboss_01", "g_m_m_armgoon_01", "g_m_m_armgoon_01"},
+        {"g_m_m_mexboss_01", "g_m_y_mexgoon_01", "g_f_y_vagos_01"},
+        {"g_m_m_mexboss_01", "g_m_y_mexgoon_01", "g_f_y_vagos_01"},
+        {"s_m_m_movprem_01", "s_m_m_highsec_01", "s_m_m_highsec_01"}
+    }
+    local model = {
+        "cognoscenti",
+        "cognoscenti",
+        "baller6"
+    }
+
+    selectedBuyer = 3
+    blips[1] = AddBlipForCoord(meetingPoint[selectedBuyer][meet])
+    print(meetingPoint[selectedBuyer][meet])
+    SetBlipColour(blips[1], 5)
+    SetBlipRoute(blips[1], true)
+    SetBlipRouteColour(blips[1], 5)
+    
+    heistInProgress = true
+
+    if player == 1 then 
+        LoadModel(model[selectedBuyer])
+
+        for i = 1, 2 do 
+            LoadModel(pedModels[selectedBuyer][i])
+        end
+        
+        vehs[3] = CreateVehicle(GetHashKey(model), meetingPoint[selectedBuyer][meet], true, false)
+        
+        for i = 1, 3 do 
+            peds[i] = CreatePedInsideVehicle(veh[3], 0, GetHashKey(pedModels[selectedBuyer][i]), -2 + i, true, false)
+        end
+    end
+    
     CreateThread(function()
-        while true do 
+        while heistInProgress do 
             Wait(1000)
-
-            SubtitleMsg("Deliver the ~g~loot~s~ to the ~y~buyer.", 1100)
+            
+            if #(GetEntityCoords(PlayerPedId()) - meetingPoint[selectedBuyer][meet]) < 5 then 
+                if IsNotClose(meetingPoint[selectedBuyer][meet], 5) then 
+                    SubtitleMsg("Wait for your team to reach the buyer", 1100)
+                else 
+                    FinishHeist(meet)
+                end
+            else 
+                SubtitleMsg("Deliver the ~g~loot~s~ to the ~y~buyer.", 1100)
+            end
         end
     end)
+end
 
-    while not IsPedInAnyVehicle(PlayerPedId(), true) do 
-        Wait(100)
+function FinishHeist(meet)
+    local buyer = 0
+    if selectedBuyer == 1 then 
+        buyer = 2
+    else 
+        buyer = selectedBuyer
     end
 
-    RemoveBlip(blips[2])
+    LoadCutscene("hs3f_all_drp" .. buyer)
+    SetCutsceneOrigin(meetingPoint[selectedBuyer][meet])
+    StartCutscene(0)
 
-    SetBlipCoords()
-    SetBlipSprite()
-    SetBlipColour()
-    --RemoveBlip(blips[i])
+    for i = 1, 3 do 
+        DeleteVehicle(vehs[i])
+    end
+
+    local arr = {}
+    if #hPlayer == 2 then 
+        arr = {"MP_3", "MP_4"}
+    elseif #hPlayer == 3 then 
+        arr = {"MP_4"}  
+    end
+    
+    if #arr > 0 then 
+        for i = 1, #arr do 
+            SetEntityVisible(GetEntityIndexOfCutsceneEntity(arr[i], 0), false, false)
+        end
+    end 
+
+    repeat Wait(100) until HasCutsceneFinished()
+
+    DoScreenFadeOut(2000)
+
+    while not IsScreenFadedOut() do 
+        Wait(10)
+    end
+
+    LoadCutscene("")
 end
+
+RegisterNetEvent("cl:casinoheist:syncMeet", Route)
 
 RegisterCommand("test_veh", function()
     LoadModel("t20")
     veh = CreateVehicle(GetHashKey("t20"), 985.31, -215.88, 70.5, 271.44, true, false)
 end)
+
+RegisterCommand("test_exit", ExitCasino, false)
