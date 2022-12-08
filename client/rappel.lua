3595d975ba9e05a62a06b96023d89d265152ef62
pindakaastosti <pindakaastosti111@gmail.com>
2022-12-08 21:51:03 +0100

    tweak(entry) rappel

--- client/rappel.lua
+++ client/rappel.lua
@@ -28,19 +28,75 @@ function Start()
     RopeStart()
 end
 
+local function HideCutProps()
+    while not DoesCutsceneEntityExist("MP_3") do 
+        Wait(10)
+    end
+
+    local arr = {}
+    if #hPlayer == 2 then 
+        arr = {"MP_3", "Player_Rope_3", "Player_Pulley_3", "MP_4", "Player_Rope_4", "Player_Pulley_4"}
+    elseif #hPlayer == 3 then 
+        arr = {"MP_4", "Player_Rope_4", "Player_Pulley_4"}
+    end
+    
+    if #arr > 0 then 
+        for i = 1, #arr do 
+            SetEntityVisible(GetEntityIndexOfCutsceneEntity(arr[i], 0), false, false)
+        end
+    end 
+
+    repeat Wait(100) until HasCutsceneFinished()
+end
+
 function RopeStart()
-    local coords = vector3(2572.341, -254.9, -64.66)
+    player = 1
+    LoadCutscene("hs3f_mul_rp1")
+    StartCutscene(0)
+
+    HideCutProps()
+
+    local coords1 = { -- func_11488
+        vector3(2572.225, -254.2, -64.8),
+        vector3(2573.575, -254.2, -64.8),
+        vector3(2571.0, -254.2, -64.8),
+        vector3(2574.8, -254.2, -64.8)
+    }
+    
+    local coords2 = { -- func_11487
+        vector3(2572.35, -255.4523, -73.3),
+        vector3(2573.575, -255.4523, -73.3),
+        vector3(2571.0, -255.4523, -73.3),
+        vector3(2574.8, -255.4523, -73.3)
+    }
 
     RopeLoadTextures()
-    SetEntityCoords(PlayerPedId(), coords - vector3(0, 0, 10.0))
 
-    local ropeId = AddRope(coords, -90.0, 90.0, -90.0, 80.0, 1, 80.0, 80.0, 1.2, false, false, true, 10.0, false, 0)
+    var3 = coords1[player]
+    var4 = coords2[player]
+    var6 = 78.0
+    var8 = vector3(-90.0, 90.0, -90.0)
 
-    TaskRappelDownWall(PlayerPedId(), coords, coords, -136.0, ropeId, "clipset@anim_heist@hs3f@ig1_rappel@male", 1)
+    local ropeId = AddRope(var3, var8, var6, 7, var6, var6, 1.2, false, false, true, 10.0, false, 0)
     N_0xa1ae736541b0fca3(ropeId, true)
-    PinRopeVertex(ropeId, (GetRopeVertexCount(ropeId) - 1), coords + vector3(0, 0, 1.0))
+
+    SetEntityCoords(PlayerPedId(), var4, true, false, false, true)
+    SetEntityHeading(PlayerPedId(), 178.5)
+
+    TaskRappelDownWall(PlayerPedId(), var3, var3, -143.0, ropeId, "clipset@anim_heist@hs3f@ig1_rappel@male", 1)
+    PinRopeVertex(ropeId, (GetRopeVertexCount(ropeId) - 1), var3)
     RopeSetUpdateOrder(ropeId, 0)
-    rope = true
+
+    while GetEntityCoords(GetHeistPlayerPed(hPlayer[1])).z > -136 or GetEntityCoords(GetHeistPlayerPed(hPlayer[2])).z > -136 or GetEntityCoords(GetHeistPlayerPed(hPlayer[3])).z > -136 or GetEntityCoords(GetHeistPlayerPed(hPlayer[4])).z > -136 do
+        Wait(100)
+    end
+
+    LoadCutscene("hs3f_mul_rp2")
+    StartCutscene(0)
+
+    HideCutProps()
+
+    --Basement()
 end
 
 CreateThread(function() 
