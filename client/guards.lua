449df3853bd38e12a0668fb9e9a3958f5df5ce88
pindakaastosti <pindakaastosti111@gmail.com>
2022-12-27 21:07:10 +0100

    feat(guard) last guard :) in the security lobby

--- client/guards.lua
+++ client/guards.lua
@@ -4,45 +4,45 @@ local guards = {
         {  
             "s_m_m_highsec_03", -- Suit
             ["coords"] = {
-                vector3(2488.01, -274.31, -70.69),
-                vector3(2480.99, -273.13, -70.96)
+                vector4(2488.01, -274.31, -70.69, 0.0),
+                vector4(2480.99, -273.13, -70.69, 0.0)
             },
 
-            ["tick"] = 1
+            ["tick"] = 2
         },
         {
             "s_m_m_highsec_03", -- Suit
             ["coords"] = {
-                vector3(2467.97, -267.99, -70.69),
-                vector3(2472.53, -270.24, -70.69)
+                vector4(2467.97, -267.99, -70.69, 0.0),
+                vector4(2472.53, -270.24, -70.69, 0.0)
             },
 
-            ["tick"] = 1
+            ["tick"] = 2
         },
         {
             "s_m_y_westsec_02", -- Work
             ["coords"] = {
-                vector3(2477.85, -270.57, -70.69),
-                vector3(2524.24, -281.16, -70.69),
-                vector3(2477.79, -279.78, -70.69)
+                vector4(2477.85, -270.57, -70.69, 0.0),
+                vector4(2524.24, -281.16, -70.69, 0.0),
+                vector4(2477.79, -279.78, -70.69, 0.0)
             },
 
-            ["tick"] = 1
+            ["tick"] = 2
         },
         {
             "s_m_y_westsec_02", -- Work
             ["coords"] = {
-                vector3(2491.72, -276.4, -70.69),
-                vector3(2524.24, -281.16, -70.69),
-                vector3(2477.79, -279.78, -70.69)
+                vector4(2491.72, -276.4, -70.69, 0.0),
+                vector4(2524.24, -281.16, -70.69, 0.0),
+                vector4(2477.79, -279.78, -70.69, 0.0)
             },
 
-            ["tick"] = 1
+            ["tick"] = 2
         },
         {
             "s_m_y_westsec_02", -- Work
             ["coords"] = {
-
+                vector4(2491.9, -262.01, -70.69, 145.0)
             },
 
             ["anim"] = {
@@ -63,14 +63,16 @@ local function SpawnPed()
     LoadModel(guards[1][1][1])
     LoadModel(guards[1][3][1])
 
-    for i = 1, 4 do 
-        activeGuards[i] = CreatePed(1, GetHashKey(guards[1][i][1]), guards[1][i]["coords"][guards[1][i]["tick"] + 1], 180.0, false --[[test]], false)
+    for i = 1, 5 do 
+        activeGuards[i] = CreatePed(1, GetHashKey(guards[1][i][1]), guards[1][i]["coords"][1], false --[[test]], false)
         blips[i] = AddBlipForEntity(activeGuards[i])
         SetBlipSprite(blips[i], 270)
-        --SetBlipShowCone(blips[i], true)
         SetBlipColour(blips[i], 1)
         SetBlipScale(blips[i], 0.75)
+        --SetBlipShowCone(blips[i], true)
     end 
+
+    
 end
 
 function StartNav(num) 
@@ -80,12 +82,12 @@ function StartNav(num)
 end
 
 function StartWalking()
-    for i = 1, #activeGuards do 
+    for i = 1, 4 do 
         CreateThread(function()
             local j = i
 
             while not IsPedDeadOrDying(activeGuards[j]) and isInSecurity do
-                TaskFollowNavMeshToCoord(activeGuards[j], guards[1][j]["coords"][guards[1][j]["tick"]], 1.0, -1, 1.0, true, 2.0)
+                TaskFollowNavMeshToCoord(activeGuards[j], guards[1][j]["coords"][guards[1][j]["tick"]].xyz, 1.0, -1, 1.0, true, 2.0)
 
                 repeat Wait(1000) until not IsPedWalking(activeGuards[j])
 
@@ -104,4 +106,3 @@ end
 RegisterCommand("test_nav", function()
     StartNav(1)
 end, false)
-
